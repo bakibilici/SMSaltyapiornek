24f9ae2168f8a713fe9898f831f9a67fdd26547f
bakibilici <49272761+bakibilici@users.noreply.github.com>
2023-02-28 20:50:37 +0300

    Add files via upload

---
+++ Send/Send.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+//declaration
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+//sıraya alan
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+//mesaj oluştur
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+//mesajı yayımla
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "hello",
+                     basicProperties: null,
+                     body: body);
+
+//console çıktısı
+Console.WriteLine($" [x] Sent {message}");
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
